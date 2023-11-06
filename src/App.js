7cf20482326b01682ec6edd0dc584982a1c0b675
VIKTOR-PC\lyuti <cxvbvcxvbg@mail.ru>
2023-11-06 15:50:57 +0300

    fetch useEffect

--- src/App.js
+++ src/App.js
@@ -5,23 +5,39 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzaItems from './assets/pizzas.json';
+// import pizzaItems from './assets/pizzas.json';
 import './scss/app.scss';
 
 function App() {
+	const [items, setItems] = React.useState([]);
+
+	React.useEffect(() => {
+		fetch(`https://653f682d9e8bd3be29e07f76.mockapi.io/items`)
+			.then((res) => {
+				return res.json();
+			})
+			.then((json) => {
+				setItems(json);
+			})
+			.catch((err) => {
+				console.warn(err);
+				alert('Error fatch API');
+			});
+	}, []);
+
 	return (
 		<div className="wrapper">
 			<Header />
 			<div className="content">
 				<div className="container">
 					<div className="content__top">
-						{/* <Categories /> */}
+						<Categories />
 						<Sort />
 					</div>
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
-						{pizzaItems.map((obj) => (
-							<PizzaBlock key={obj.name} {...obj}/>
+						{items.map((obj) => (
+							<PizzaBlock key={obj.name} {...obj} />
 						))}
 					</div>
 				</div>
