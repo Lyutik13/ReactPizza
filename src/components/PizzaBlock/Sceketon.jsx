8779fea1ae9d8810c184b698bd2a8a3e79c34714
VIKTOR-PC\lyuti <cxvbvcxvbg@mail.ru>
2023-11-10 18:47:53 +0300

    react-router-dom / sceleton

---
+++ src/components/PizzaBlock/Sceketon.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Sceketon = (props) => (
+	<ContentLoader
+		className="pizza-block"
+		speed={0}
+		width={280}
+		height={466}
+		viewBox="0 0 280 466"
+		backgroundColor="rgb(255,223,140,0.3)"
+		foregroundColor="#ecebeb"
+		{...props}>
+		<rect x="0" y="267" rx="5" ry="5" width="280" height="20" />
+		<rect x="5" y="314" rx="5" ry="5" width="268" height="85" />
+		<rect x="5" y="425" rx="0" ry="0" width="85" height="22" />
+		<rect x="117" y="414" rx="20" ry="20" width="156" height="43" />
+		<circle cx="135" cy="121" r="121" />
+	</ContentLoader>
+);
+
+export default Sceketon;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Sceketon from '../components/PizzaBlock/Sceketon';
+
+export const Home = () => {
+	const [items, setItems] = React.useState([]);
+	const [loading, setLoading] = React.useState(true);
+
+	React.useEffect(() => {
+		setLoading(true);
+
+		fetch(`https://653f682d9e8bd3be29e07f76.mockapi.io/items`)
+			.then((res) => {
+				return res.json();
+			})
+			.then((json) => {
+				setItems(json);
+			})
+			.catch((err) => {
+				console.warn(err);
+				alert('Error fatch API');
+			})
+			.finally(() => setLoading(false));
+	}, []);
+  
+	return (
+		<>
+			<div className="content__top">
+				<Categories />
+				<Sort />
+			</div>
+			<h2 className="content__title">Все пиццы</h2>
+			<div className="content__items">
+				{(loading ? [...Array(8)] : items).map((obj, i) =>
+					loading ? <Sceketon key={i} /> : <PizzaBlock key={obj.name} {...obj} />,
+				)}
+			</div>
+		</>
+	);
+};
