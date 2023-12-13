fecc9e6e2c85a2bcd054a854d63184ceea8f0e85
VIKTOR-PC\lyuti <cxvbvcxvbg@mail.ru>
2023-12-13 18:24:22 +0300

    rrd - useLocation / useParams

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import { Home } from './pages/Home';
 import { Cart } from './pages/Cart';
+import OneInfoPizza from './components/OneInfoPizza/OneInfoPizza';
 import NotFound from './pages/NotFound';
 
 // import pizzaItems from './assets/pizzas.json';
@@ -22,6 +23,7 @@ function App() {
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/cart" element={<Cart />} />
+						<Route path="/item/:id" element={<OneInfoPizza />} />
 						<Route path="*" element={<NotFound />} />
 					</Routes>
 				</div>
@@ -31,5 +33,3 @@ function App() {
 }
 
 export default App;
-
-//44:40
---
+++ src/components/OneInfoPizza/OneInfoPizza.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import style from './OneInfoPizza.module.scss';
+
+export const OneInfoPizza = () => {
+	let { id } = useParams();
+	const navigate = useNavigate();
+	const [onePizza, setOnePizza] = useState('');
+
+	useEffect(() => {
+		async function fetchOnePizza() {
+			try {
+				const { data } = await axios.get(`https://653f682d9e8bd3be29e07f76.mockapi.io/items/${id}`);
+				setOnePizza(data);
+			} catch (error) {
+				alert('error fetchOnePizza');
+				console.log(error);
+				navigate('/');
+			}
+		}
+
+		fetchOnePizza();
+    // eslint-disable-next-line
+	}, []);
+
+	if (!onePizza) {
+		return (
+			<div style={{ textAlign: 'center' }} className="container">
+				loading...
+			</div>
+		);
+	}
+
+	return (
+		<div className={`container ${style.oneInfoPizza}`}>
+			<div>
+				<img className={style.oneInfoPizza__img} src={onePizza.imageUrl} alt="Pizza" />
+			</div>
+			<div className={style.oneInfoPizza__desc}>
+				<div className={style.textWrapper}>
+					<h4 className={style.h4} style={{ fontSize: '36px' }}>
+						{onePizza.name}
+					</h4>
+					<div className={style.span} style={{ fontSize: '24px', color: '#fe5f1e' }}>
+						от {onePizza.price} ₽
+					</div>
+				</div>
+				<div>
+					<Link to={'/'} className="button button--black">
+						<span>Вернуться назад</span>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default OneInfoPizza;
